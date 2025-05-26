0d8caae6804cfb68524c9b953af072ab4ea87c9a
Maximus <mail@maximuscoleman.com>
2025-05-27 00:18:39 +0100

    The Items class fetch_fromapi method will now retrieve a configurable amount of items from the api ep.

--- aq3d_api/items/containers.py
+++ aq3d_api/items/containers.py
@@ -1,9 +1,14 @@
+import requests
+from requests import JSONDecodeError
+
+from aq3d_api.items.item import Item
 from aq3d_api.updater.api_updater import APIUpdater
 
 
 class Items(APIUpdater):
     api_url = "https://game.aq3d.com/api/Game/GetItems"
     param_key = "IDs"
+    api_bulk_req_limit = 200
 
     def __init__(self,
                  items = None,
@@ -12,7 +17,49 @@ class Items(APIUpdater):
                  auto_update_fromapi: bool = False,
                  update_interval: int = 60
                  ):
+        self.items = items
+        self.api_item_max = api_item_max
+        if fromapi:
+            self.items = self.__fetch_fromapi()
+
         super().__init__(auto_update_fromapi, update_interval)
 
-    def __fetch_fromapi(self) -> tuple | None:
-        pass
+    def __fetch_fromapi(self) -> list | None:
+        bulk_req_limit = self.api_bulk_req_limit
+        item_limit = self.api_item_max
+
+        get_reqs_needed = (
+            (item_limit // bulk_req_limit), (item_limit % bulk_req_limit)
+        )
+
+        items = []
+        max_iteration = get_reqs_needed[0]
+        if get_reqs_needed[-1] > 0:
+            max_iteration += 1
+
+        for req_num in range(1, max_iteration + 1):
+            start_index = (bulk_req_limit * (req_num - 1)) + 1\
+                if req_num > 1 else 1
+
+            end_index = (start_index + bulk_req_limit)
+            if req_num >= max_iteration:
+                end_index = (start_index + get_reqs_needed[-1])
+
+            params = {self.param_key: [
+                    key_id for key_id in range(start_index, end_index)
+                ]
+            }
+
+            response = requests.post(self.api_url, params=params)
+            try:
+                raw_items = response.json()
+                items += [Item.create_raw(item) for item in raw_items]
+            except JSONDecodeError:
+                raise JSONDecodeError(
+                    "Invalid item data was retrieved from the api."
+                )
+
+        return items
+
+    def __iter__(self) -> iter:
+        return iter(self.items)
--- examples/example_items.py
+++ examples/example_items.py
@@ -1,3 +1,3 @@
 from aq3d_api.items.containers import Items
 
-items = Items()
+items = Items(fromapi=True, api_item_max=300)

6bb908fbf38296c44a5d517c73697ab6ace6a50a
Maximus <mail@maximuscoleman.com>
2025-05-28 17:43:53 +0100

    Added the dialog API endpoints to the endpoints Enum class.

--- aq3d_api/api/endpoints.py
+++ aq3d_api/api/endpoints.py
@@ -2,7 +2,7 @@
 
 from enum import Enum
 
-base_api_url = "https://game.aq3d.com/api/Game"
+base_api_url = "https://game.aq3d.com/api"
 
 
 class Endpoints(Enum):
@@ -10,6 +10,7 @@ class Endpoints(Enum):
     The Enum class for API URLs.
     """
 
-    GET_SERVERS = f"{base_api_url}/GetServerList", ""
-    GET_ITEMS = f"{base_api_url}/GetItems", "IDs"
-    GET_MAPS = f"{base_api_url}/GetDungeons", "IDs"
+    GET_SERVERS = f"{base_api_url}/Game/GetServerList", ""
+    GET_ITEMS = f"{base_api_url}/Game/GetItems", "IDs"
+    GET_MAPS = f"{base_api_url}/Game/GetDungeons", "IDs"
+    GET_DIALOGS = f"{base_api_url}/Utilities/GetDialogueByID", "dialogueID"
